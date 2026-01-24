61e93cf09983d334a1fcd01c7423b84a5dcb39e3
kholomeev <holomeev2011@gmail.com>
2026-01-24 14:20:43 +0300

    Binding, ConstraintLayout, размеры в dp/sp, текст в ресурсах

--- app/src/main/java/com/kholomeev/dice/MainActivity.kt
+++ app/src/main/java/com/kholomeev/dice/MainActivity.kt
@@ -7,24 +7,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.Random
+import com.kholomeev.dice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener({view ->
-            val textView: TextView = findViewById(R.id.textView)
+        binding.button.setOnClickListener { view ->
             val digit = (1..6).random()
-            textView.text = "$digit"
-        })
+            binding.textView.text = "$digit"
+        }
     }
 }
